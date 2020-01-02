03d5455eaeeda1330fcd941c268c58dee68beb60
Anthony Holder <anthonyh202x@gmail.com>
2020-01-02 16:35:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def load_library (file_path)
   YAML.load_file(file_path)
-  return {'get_meaning' : nil , 'get_emoticion' : nil}
+  return {"get_meaning" : nil , "get_emoticion" : nil}
 end
 
 def get_japanese_emoticon
